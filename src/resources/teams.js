3a3fa034fe281d54610f5a8de8b78bbcf0ca1e44
3Labs <tri3labs@gmail.com>
2021-05-28 17:48:30 +0700

    Dashboard Creation

---
+++ src/resources/teams.js
@@ -0,0 +1,22 @@
+export const teams = [
+    {
+        'teamID': 1,
+        'teamName': 'Indonesia',
+        'teamColor': '#FF6358'
+    },
+    {
+        'teamID': 2,
+        'teamName': 'New Zealand',
+        'teamColor': '#F7C62F'
+    },
+    {
+        'teamID': 3,
+        'teamName': 'Malaysia',
+        'teamColor': '#55AB1D'
+    },
+    {
+        'teamID': 4,
+        'teamName': 'Philippines',
+        'teamColor': '#28B4C8'
+    }
+];
