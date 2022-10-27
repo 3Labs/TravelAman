71bc10602b2b51ff81f2192bdb3a4a430c67b2d9
3Labs <tri3labs@gmail.com>
2022-10-28 00:06:46 +0700

    Update Info.jsx

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -6,8 +6,7 @@ import { ButtonGroup, Button } from '@progress/kendo-react-buttons';
 const Info = () => {
   return (
     <div id="Info" className="card-buttons">
-      <Button>Hai</Button >
-      <Text>Sentiment Analysis</Text>
+      <Button>Sentiment Analysis</Button >      
     </div>
     );
   }
